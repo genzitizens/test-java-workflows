1e908a06d4b557892dc08a1ca4dc57efe8f80779
mediocreasian <exelbert2010@gmail.com>
2025-04-27 00:38:21 +0800

    Updated tests and CI template

--- src/test/java/com/example/expense/ExpenseApplicationTests.java
+++ src/test/java/com/example/expense/ExpenseApplicationTests.java
@@ -2,7 +2,11 @@ package com.example.expense;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@SpringBootTest
+@EnableAutoConfiguration(exclude = {
+    org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
 @SpringBootTest
 class ExpenseApplicationTests {
 
